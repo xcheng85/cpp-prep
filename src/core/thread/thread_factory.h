a7363cdd61b19777f97ff05f90d96b6e71597189
Shane Cheng <shane85cheng@gmail.com>
2024-05-05 15:06:18 -0500

    using future-based task scheduler

--- src/core/thread/thread_factory.h
+++ src/core/thread/thread_factory.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <memory>
 #include <thread>
+#include <future>
 
 namespace core::system
 {
@@ -14,13 +15,33 @@ namespace core::system
         return std::make_unique<std::thread>(std::forward<F>(f), std::forward<Args>(args)...);
     }
 
+    // thread-based
     template <typename F, typename... Args>
     inline auto CreateThreadGroup(size_t numThreads, F &&f, Args &&...args)
     {
         std::vector<unique_ptr<thread>> res;
-        for(size_t i = 0; i < numThreads; ++i){
+        for (size_t i = 0; i < numThreads; ++i)
+        {
             res.emplace_back(std::make_unique<std::thread>(std::forward<F>(f), std::forward<Args>(args)...));
         }
+        return res;
+    }
+
+    // future task-based
+    template <typename F, typename... Args>
+    inline auto SpawnTask(F &&f, Args &&...args)
+    {
+        // typename is a must due to a template
+        using ReturnType = typename std::result_of<F(Args...)>::type;
+        // std::packaged_task<ReturnType(Args...)> task(f, args...);
+        // package_task only took function,
+        // parameters is provided when it is executed in line 42.
+        std::packaged_task<ReturnType(Args...)> task(f);
+        auto res{task.get_future()};
+        //task, lvalue must move
+        std::thread t{std::move(task), std::forward<Args>(args)...};
+        t.detach();
+        // compiler optim
         return res;
     }
 }
---
+++ src/multithreading/fp_concurrent/bootstrap.cpp
@@ -0,0 +1,90 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <format>
+#include <chrono>
+#include <thread_factory.h>
+#include <logger.h>
+#include <queuethreadsafe.h>
+#include <future> //packaged_task<>
+#include <functional>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <utility> // for std::pair
+#include <tuple>   // for std::tuple
+#include <random>
+#include <algorithm> // partition
+#include <concepts>
+#include <iterator> // ostream_iterator
+
+using namespace std::chrono;
+using namespace core::utility;
+using namespace core::system;
+
+// signature of fp
+
+// why list ?
+// the list.splice(move) between two lists can avoid copying
+
+// pure function
+template <typename T>
+    requires three_way_comparable<T>
+std::list<T> quickSort(std::list<T> input) // no const due to splice, no ref due to splice
+{
+    std::list<T> res;
+
+    // recursive end condition
+    if (input.size() == 0)
+    {
+        return res;
+    }
+
+    // dst itr,
+    // source list
+    // src itr of source list
+    // a move for only one element pointed by src itr
+
+    // only transfer the pivot
+    // must guard against input.
+    res.splice(res.begin(), input, input.begin());
+
+    // select the first element as pivot
+    const auto &pivot = *(res.begin());
+    // quickly divide into two parts using stl algorithm partition
+
+    auto partitionItr = std::partition(input.begin(), input.end(), [&](const auto &t)
+                                       { return t < pivot; });
+
+    // move the lower part out using range
+    std::list<T> lowerPart;
+    lowerPart.splice(lowerPart.begin(), input, input.begin(), partitionItr);
+
+    // recursive, divide and conquer
+    // avoid copy and also marks the lowerPart should not be used after next line.
+    auto sortedLowerPart{quickSort(std::move(lowerPart))};
+    auto sortedHigherPart{quickSort(std::move(input))};
+
+    // merge two sorted part with pivot
+    // the res so far only has pivot one value
+    // move after the pivot
+    res.splice(res.end(), sortedHigherPart);
+    // move before the pivot
+    res.splice(res.begin(), sortedLowerPart);
+    return res;
+}
+
+int main()
+{
+    auto &logger = DefaultLogger::Instance();
+    logger.setLogLevel(LogLevel::DEBUG);
+
+    std::list<int> unsorted({1, 30, -4, 3, 5, -4, 1, 6, -8, 2, -5, 64, 1, 92});
+    // std::copy(std::begin(sorted), std::end(sorted), std::ostream_iterator<int>(std::cout, " "));
+    // must move unsorted
+    auto f = SpawnTask(quickSort<int>, std::move(unsorted));
+    const auto sorted = f.get();
+    std::copy(std::begin(sorted), std::end(sorted), std::ostream_iterator<int>(std::cout, " "));
+
+    return 0;
+}

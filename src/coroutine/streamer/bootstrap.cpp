a4db72cbf2095e2db69a805a9bdd95aa3fbb8008
Shane Cheng <shane85cheng@gmail.com>
2024-12-15 14:58:23 -0800

    fixed the template-based impl

--- src/coroutine/streamer/bootstrap.cpp
+++ src/coroutine/streamer/bootstrap.cpp
@@ -10,152 +10,152 @@
 using namespace std::string_literals; // for ""s
 using namespace std::chrono_literals;
 
-// // network i/o, node.js style
-// template <typename CoroToCallerType, typename CallerToCoroType>
-// struct AsyncStreamer
-// {
-//     struct promise_type
-//     {
-//         // minimum requirement
-//         void return_void() noexcept {}
+// network i/o, node.js style
+template <typename CoroToCallerType, typename CallerToCoroType>
+struct AsyncStreamer
+{
+    struct promise_type
+    {
+        // minimum requirement
+        void return_void() noexcept {}
 
-//         std::suspend_always initial_suspend() noexcept
-//         {
-//             return {};
-//         }
+        std::suspend_always initial_suspend() noexcept
+        {
+            return {};
+        }
 
-//         std::suspend_always final_suspend() noexcept
-//         {
-//             return {};
-//         }
+        std::suspend_always final_suspend() noexcept
+        {
+            return {};
+        }
 
-//         void unhandled_exception() noexcept {}
+        void unhandled_exception() noexcept {}
 
-//         AsyncStreamer get_return_object() noexcept
-//         {
-//             // another ctor
-//             return AsyncStreamer{*this};
-//         }
+        AsyncStreamer get_return_object() noexcept
+        {
+            // another ctor
+            return AsyncStreamer{*this};
+        }
 
-//         // extra saurce: string as contract
-//         // responding to co_yield string_type
-//         std::suspend_always yield_value(CoroToCallerType payload) noexcept
-//         {
-//             // when yield the frame state is updated.
-//             // std::cout << "yield_value: " << payload << std::endl;
-//             _coroutineToCaller = payload;
-//             return {};
-//         }
+        // extra saurce: string as contract
+        // responding to co_yield string_type
+        std::suspend_always yield_value(CoroToCallerType payload) noexcept
+        {
+            // when yield the frame state is updated.
+            // std::cout << "yield_value: " << payload << std::endl;
+            _coroutineToCaller = payload;
+            return {};
+        }
 
-//         const auto &coroutineToCaller() const noexcept
-//         {
-//             // std::cout << "coroutineToCaller: " << _coroutineToCaller << std::endl;
-//             return _coroutineToCaller;
-//         }
+        const auto &coroutineToCaller() const noexcept
+        {
+            // std::cout << "coroutineToCaller: " << _coroutineToCaller << std::endl;
+            return _coroutineToCaller;
+        }
 
-//         const auto &callerToCoroutine() const noexcept
-//         {
-//             return _callerToCoroutine;
-//         }
+        const auto &callerToCoroutine() const noexcept
+        {
+            return _callerToCoroutine;
+        }
 
-//         auto &callerToCoroutine() noexcept
-//         {
-//             return _callerToCoroutine;
-//         }
+        auto &callerToCoroutine() noexcept
+        {
+            return _callerToCoroutine;
+        }
 
-//         // transform payload (string type) to awaiter object
-//         // awaiter object: must implement: await_ready, await_resume, await_suspend
+        // transform payload (string type) to awaiter object
+        // awaiter object: must implement: await_ready, await_resume, await_suspend
 
-//         // await_transfrom is the specs behind: co_await
-//         auto await_transform(CallerToCoroType)
-//         {
-//             struct awaiter
-//             {
-//                 awaiter(promise_type &promise)
-//                     : _promise(promise)
-//                 {
-//                 }
-//                 // check whether coroutine is suspended
-//                 bool await_ready() const noexcept
-//                 {
-//                     // only ready if _coroutineToCaller has value
-//                     return _promise.callerToCoroutine().has_value();
-//                 }
+        // await_transfrom is the specs behind: co_await
+        auto await_transform(CallerToCoroType)
+        {
+            struct awaiter
+            {
+                awaiter(promise_type &promise)
+                    : _promise(promise)
+                {
+                }
+                // check whether coroutine is suspended
+                bool await_ready() const noexcept
+                {
+                    // only ready if _coroutineToCaller has value
+                    return _promise.callerToCoroutine().has_value();
+                }
 
-//                 CoroToCallerType await_resume() const noexcept
-//                 {
-//                     // must already passed the await_ready check
-//                     assert(_promise.callerToCoroutine().has_value());
-//                     // exchange return the old value and set the new value
-//                     // nullopt is for std::optional
-//                     //return *_promise.callerToCoroutine();
-//                    return *std::exchange(_promise.callerToCoroutine(), std::nullopt);
-//                 }
+                CallerToCoroType await_resume() const noexcept
+                {
+                    // must already passed the await_ready check
+                    assert(_promise.callerToCoroutine().has_value());
+                    // exchange return the old value and set the new value
+                    // nullopt is for std::optional
+                    //return *_promise.callerToCoroutine();
+                   return *std::exchange(_promise.callerToCoroutine(), std::nullopt);
+                }
 
-//                 void await_suspend(std::coroutine_handle<promise_type>) const noexcept
-//                 {
-//                 }
+                void await_suspend(std::coroutine_handle<promise_type>) const noexcept
+                {
+                }
 
-//             private:
-//                 promise_type &_promise;
-//             };
+            private:
+                promise_type &_promise;
+            };
 
-//             return awaiter(*this);
-//         }
+            return awaiter(*this);
+        }
 
-//     private:
-//         // coutrine frame state is here
-//         // for yield coroutine:
-//         std::optional<CallerToCoroType> _callerToCoroutine;
-//         // for wait coroutine:
-//         CoroToCallerType _coroutineToCaller;
-//     };
+    private:
+        // coutrine frame state is here
+        // for yield coroutine:
+        std::optional<CallerToCoroType> _callerToCoroutine;
+        // for wait coroutine:
+        CoroToCallerType _coroutineToCaller;
+    };
 
-//     // static method of template class and copy ctor
-//     explicit AsyncStreamer(promise_type &promise)
-//         : _handle{
-//               std::coroutine_handle<promise_type>::from_promise(promise)}
-//     {
-//     }
+    // static method of template class and copy ctor
+    explicit AsyncStreamer(promise_type &promise)
+        : _handle{
+              std::coroutine_handle<promise_type>::from_promise(promise)}
+    {
+    }
 
-//     ~AsyncStreamer() noexcept
-//     {
-//         if (!_handle)
-//         {
-//             _handle.destroy();
-//         }
-//     }
+    ~AsyncStreamer() noexcept
+    {
+        if (!_handle)
+        {
+            _handle.destroy();
+        }
+    }
 
-//     // for coroutine handle: here contract type is string
-//     // get: one way communication from coroutine to the caller
-//     CoroToCallerType get()
-//     {
-//         if (!_handle.done())
-//         {
-//             _handle.resume();
-//         }
-//         // coroutine handle is created through from_promise;
-//         // so it is logical to access the payload
-//         // promise can access
-//         return _handle.promise().coroutineToCaller();
-//     }
+    // for coroutine handle: here contract type is string
+    // get: one way communication from coroutine to the caller
+    CoroToCallerType get()
+    {
+        if (!_handle.done())
+        {
+            _handle.resume();
+        }
+        // coroutine handle is created through from_promise;
+        // so it is logical to access the payload
+        // promise can access
+        return _handle.promise().coroutineToCaller();
+    }
 
-//     // put: one way communication from caller to the coroutine
-//     void request(CallerToCoroType payload)
-//     {
-//         // everything is going through coroutine handler
-//         _handle.promise().callerToCoroutine() = payload;
-//         if (!_handle.done())
-//         {
-//             _handle.resume();
-//         }
-//     }
+    // put: one way communication from caller to the coroutine
+    void request(CallerToCoroType payload)
+    {
+        // everything is going through coroutine handler
+        _handle.promise().callerToCoroutine() = payload;
+        if (!_handle.done())
+        {
+            _handle.resume();
+        }
+    }
 
-// private:
-//     // coroutine handle: specialization of template std::coroutine_handle
-//     // it has resume, done, destroy
-//     std::coroutine_handle<promise_type> _handle;
-// };
+private:
+    // coroutine handle: specialization of template std::coroutine_handle
+    // it has resume, done, destroy
+    std::coroutine_handle<promise_type> _handle;
+};
 
 struct NetworkClient
 {
@@ -304,7 +304,7 @@ private:
 
 // INPUT: CHAR
 // OUTPUT: INT
-NetworkClient createStreamingClient()
+AsyncStreamer<std::string, char> createStreamingClient()
 {
     while (true)
     {

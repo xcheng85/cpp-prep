09aaff62f56db08d63e231b816d523cff5622bf4
Shane Cheng <shane85cheng@gmail.com>
2025-03-10 08:05:25 -0700

    custom impl of smarter pointer

--- src/moderncpp/smartpointer.h
+++ src/moderncpp/smartpointer.h
@@ -5,6 +5,168 @@
 
 namespace SmartPointers
 {
+    // interface for smart point (both unique_ptr and shared_ptr)
+    // Dereferencing (operator *)
+    // Indirection (operator ->)
+
+    // unique_ptr: not copyable
+
+    template <typename T>
+    class CustomUniquePtr
+    {
+    public:
+        // 2nd ctor of unique_ptr besides make_unique
+        CustomUniquePtr() = delete;
+        explicit CustomUniquePtr(T *rawPtr)
+            : _ptr{rawPtr}
+        {
+            LOG_I("CustomUniquePtr::ctor");
+        }
+        CustomUniquePtr(const CustomUniquePtr &other) = delete;
+        CustomUniquePtr &operator=(const CustomUniquePtr &other) = delete;
+        CustomUniquePtr(CustomUniquePtr &&other) noexcept
+        {
+            LOG_I("CustomUniquePtr::rvalue copy ctor");
+            _ptr = std::move(other._ptr);
+        }
+        CustomUniquePtr &operator=(CustomUniquePtr &&other) noexcept
+        {
+            LOG_I("CustomUniquePtr::rvalue =");
+            _ptr = std::move(other._ptr);
+            return *this;
+        }
+
+        ~CustomUniquePtr()
+        {
+            LOG_I("CustomUniquePtr::dtor");
+            delete _ptr;
+        }
+
+        T &operator*()
+        {
+            return *_ptr;
+        }
+
+        T *operator->()
+        {
+            return _ptr;
+        }
+
+    private:
+        T *_ptr;
+    };
+
+    // Single Responsibility
+    class RefCounter
+    {
+
+    public:
+        void AddRef()
+        {
+            _count++;
+        }
+
+        int Release()
+        {
+            LOG_I("RefCounter::Release");
+            return --_count;
+        }
+
+        inline auto Count() const
+        {
+            return _count;
+        }
+
+    private:
+        int _count{0};
+    };
+
+    template <typename T>
+    class CustomSharedPtr
+    {
+    public:
+        CustomSharedPtr()
+        {
+            _refCounter = new RefCounter();
+            _refCounter->AddRef();
+        }
+
+        CustomSharedPtr(T *ptr)
+            : _ptr(ptr)
+        {
+            _refCounter = new RefCounter();
+            _refCounter->AddRef();
+        }
+
+        // key: shared_ptr is copyable
+        // copy of pointer and copy of data
+        CustomSharedPtr(const CustomSharedPtr &other)
+            : _ptr(other._ptr), _refCounter(other._refCounter)
+        {
+            _refCounter->AddRef();
+        }
+        // most critical, old memory could be released.
+        CustomSharedPtr &operator=(const CustomSharedPtr &other)
+        {
+            if (this != &other)
+            {
+                if (_refCounter->Release() == 0)
+                {
+                    delete _ptr;
+                    delete _refCounter;
+                }
+
+                _ptr = other._ptr;
+                _refCounter = other._refCounter;
+                // increase counter
+                _refCounter->AddRef();
+            }
+            return *this;
+        }
+
+        ~CustomSharedPtr()
+        {
+            if (_refCounter->Release() == 0)
+            {
+                delete _ptr;
+                delete _refCounter;
+            }
+        }
+
+        T &operator*()
+        {
+            return *_ptr;
+        }
+
+        T *operator->()
+        {
+            return _ptr;
+        }
+
+        size_t use_count() const
+        {
+            return _refCounter->Count();
+        }
+
+    private:
+        T *_ptr{nullptr};
+        // composition as strategy pattern.
+        RefCounter *_refCounter{nullptr};
+    };
+
+    void testCustomUniquePtr()
+    {
+        CustomUniquePtr<int> p(new int);
+    }
+
+    void testCustomSharedPtr()
+    {
+        CustomSharedPtr<int> p(new int);
+        CustomSharedPtr<int> p2(p);
+        CustomSharedPtr<int> p3 = (p2);
+        LOG_I(p.use_count());
+    }
+
     // with customDeleter, c-style database
     // for example:
     // vkCreateCommandPool
